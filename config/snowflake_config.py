b901abf2d043fe5661718fb54eb561f409e95834
Mohammad Parvez <parvez2114@gmail.com>
2025-06-02 00:24:53 +0530

    Add config.py to .gitignore

--- config/snowflake_config.py
+++
@@ -1,9 +0,0 @@
-SNOWFLAKE_CONFIG = {
-    "account": "JYSBZAB-MM52635",
-    "user": "PARVEZ",
-    "password": "aEUR95dcYhJ9drs",
-    "role": "ACCOUNTADMIN",
-    "warehouse": "COMPUTE_WH",
-    "database": "RESUME_PROJECT",
-    "schema": "PUBLIC"
-}
